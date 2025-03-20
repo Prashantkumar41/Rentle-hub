096db2a4fd44509af347cd7a9d6c59248431e60e
Prashant kumar <prashantkumarsw91@gmail.com>
2025-03-20 15:34:55 +0530

    app.js

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ const flash  = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
+const serverless = require("serverless-http");
 
 
 main()
@@ -153,7 +154,6 @@ app.use((err, req, res, next) => {
 // });
 
 
-const serverless = require("serverless-http");
 module.exports = serverless(app);
 
 
